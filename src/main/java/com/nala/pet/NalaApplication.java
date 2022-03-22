81cf482d96783dcd8fb62f96c4cdc3ae125f2698
Geronimo Velasco <masafesio.10@gmail.com>
2022-03-21 21:38:27 -0500

    first commit

---
+++ src/main/java/com/nala/pet/NalaApplication.java
@@ -0,0 +1,13 @@
+package com.nala.pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nala/pet/NalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nala.pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
